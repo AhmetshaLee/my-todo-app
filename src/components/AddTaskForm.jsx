22e4634571c28cccc60a0f6c37a56d3120c48e2b
Akhmetsha Sharipov <scharipov.arsen2013@yandex.ru>
2026-05-02 20:08:45 +0500

    refactor: разделен интерфейс на отдельные компоненты (формы, список, UI-элементы)

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,13 @@
+import Button from "./ui/Button"
+import Field from "./ui/Field"
+
+const AddTaskForm = () => {
+  return (
+    <form className='todo__form'>
+      <Field />
+      <Button />
+    </form>
+  )
+}
+
+export default AddTaskForm
