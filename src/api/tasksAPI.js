a04bf6adb01783c4623a93feb967f2fd370cd40d
Akhmetsha Sharipov <scharipov.arsen2013@yandex.ru>
2026-05-18 18:21:02 +0500

    feat: добавлен метод в tasksAPI для получения задачи по id

--- src/api/tasksAPI.js
+++ src/api/tasksAPI.js
@@ -9,6 +9,10 @@ const tasksAPI = {
     return fetch(BASE_URL).then((response) => response.json())
   },
 
+  getById: (id) => {
+    return fetch(`${BASE_URL}/${id}`).then((response) => response.json())
+  },
+
   add: (task) => {
     return fetch(BASE_URL, {
       method: "POST",
