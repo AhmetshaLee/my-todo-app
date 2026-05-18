612d304c16ab98420b3a5473d01025bcf9dedb3e
Akhmetsha Sharipov <scharipov.arsen2013@yandex.ru>
2026-05-18 15:53:36 +0500

    feat: добавлен файл tasksAPI с вынесенной логикой fetch запросов

---
+++ src/api/tasksAPI.js
@@ -0,0 +1,39 @@
+const BASE_URL = "http://localhost:3001/tasks"
+
+const headers = {
+  "Content-Type": "application/json",
+}
+
+const tasksAPI = {
+  getAll: () => {
+    return fetch(BASE_URL).then((response) => response.json())
+  },
+
+  add: (task) => {
+    return fetch(BASE_URL, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(task),
+    }).then((response) => response.json())
+  },
+
+  delete: (id) => {
+    return fetch(`${BASE_URL}/${id}`, {
+      method: "DELETE",
+    })
+  },
+
+  deleteAll: (ids) => {
+    return Promise.all(ids.map((id) => tasksAPI.delete(id)))
+  },
+
+  toggleComplete: (id, isDone) => {
+    return fetch(`${BASE_URL}/${id}`, {
+      method: "PATCH",
+      headers,
+      body: JSON.stringify({ isDone }),
+    })
+  },
+}
+
+export default tasksAPI
