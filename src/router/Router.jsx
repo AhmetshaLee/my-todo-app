acd61397699411c9fc3c5e93e9dba84b42bdbff7
Akhmetsha Sharipov <scharipov.arsen2013@yandex.ru>
2026-05-19 13:10:18 +0500

    refactor: переделан роутер с хардкод id на явный матчинг по шаблону /tasks/:id

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,14 @@ import { useRoute } from "./useRoute"
 const Router = (props) => {
   const { routes } = props
   const path = useRoute()
+
+  if (path.startsWith("/tasks/")) {
+    const id = path.replace("/tasks/", "")
+    const TaskPage = routes["/tasks/:id"]
+
+    return <TaskPage params={{ id }} />
+  }
+
   const Page = routes[path] ?? routes["*"]
 
   return <Page />
