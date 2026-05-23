8ec95336be54302d3f43f6fee8dcf23e4d656fa5
Akhmetsha Sharipov <scharipov.arsen2013@yandex.ru>
2026-05-23 18:10:41 +0500

    refactor: доработан слой pages

--- src/pages/TasksPage/TasksPage.jsx
+++ src/pages/TasksPage/TasksPage.jsx
@@ -1,5 +1,5 @@
-import Todo from "../components/Todo/Todo"
-import { TasksProvider } from "../context/TasksProvider"
+import Todo from "../../components/Todo/Todo"
+import { TasksProvider } from "../../context/TasksProvider"
 
 const TasksPage = () => {
   return (
